5ca85a2f3200d118e976c831b334363dd47bf349
katherine <katherine.malleck@stu.ocsb.ca>
2026-02-17 13:07:00 -0500

    changed perimeter and area

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Katherine
+ * Created on: Feb 2026
+ * This program will show the area and perimeter of a shape with certain dimensions
 */
 
-basic.showString('Hello, World!')
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
+basic.showString('The perimeter would be:' + (5+5+3+3).toString() + ' cm.')
+basic.showString('The area would be:' + (5*3).toString() + ' cm^2.')
+
+
+
